e0e5cb604e770b8edb3e43b9478d520d86ad6696
Caio Quirino <caioquir@gmail.com>
2025-09-28 22:20:39 -0300

    Alterando a base do home para ficar igual ao logistica

---
+++ Project/index/home.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+    const nomeFixo = 'Admin'; 
+    
+    welcomeUser(nomeFixo)
+});
+
+function welcomeUser(username)
+{
+    const user = document.getElementById('user');
+
+    if (user) 
+        user.textContent = 'Seja bem vindo ' + username + '!';
+    else
+        console.error("Erro: Elemento com ID 'user' não encontrado!");
+}
