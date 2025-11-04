4206019bd96fbfabe5af2ef35b98bd89a3885639
Paulo Henrique Dutra dos Santos Tavares <paulodutra247@gmail.com>
2025-11-04 11:38:20 -0300

    add script.js

---
+++ Project/login/script.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("loginForm");
+  const mensagem = document.getElementById("mensagem");
+
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById("login").value;
+    const senha = document.getElementById("senha").value;
+
+    mensagem.style.display = "block";
+    mensagem.textContent = "Entrando...";
+
+    try {
+      const response = await fetch("http://localhost:3000/usuarios/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, senha })
+      });
+
+      const data = await response.json();
+      if (response.ok && data.success) {
+        mensagem.textContent = "Login realizado!";
+        localStorage.setItem("token", data.token);
+        window.location.href = "../index/home.html";
+      } else {
+        mensagem.textContent = data.message || "Email ou senha inválidos!";
+      }
+    } catch (error) {
+      mensagem.textContent = "Erro ao conectar ao servidor.";
+      console.error(error);
+    }
+  });
+});
